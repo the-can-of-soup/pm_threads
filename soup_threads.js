fbc8012a8f65f747698fde9a177982386f0244fc
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-02-24 01:06:02 -0500

    to-do switcheroo

--- soup_threads.js
+++ soup_threads.js
@@ -10,8 +10,8 @@
 
 // TO-DO
 //
-// - Make "__name__" thread var display a label in reporter bubble header
 // - Make thread vars stored in a map instead of an object
+// - Make "__name__" thread var display a label in reporter bubble header
 // - Add "super mutator shenanagins" so that atomic forever loses its end cap if an "escape loop" block is present inside it
 // - Figure out *exactly* what happens when a hat block is restarted
 // - Figure out *exactly* what happens when an async block is run
